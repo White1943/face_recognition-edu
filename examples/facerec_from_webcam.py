ec99892e4532d96ca70e7d51c4970135320b2ab9
Prashant  Choudhary <81732236+Mr-Dark-debug@users.noreply.github.com>
2024-05-29 22:27:58 +0530

    Update facerec_from_webcam.py updated the grammer in line 40,44 and 46

--- examples/facerec_from_webcam.py
+++ examples/facerec_from_webcam.py
@@ -37,11 +37,11 @@ while True:
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
-    # Find all the faces and face enqcodings in the frame of video
+    # Find all the faces and face encodings in the frame of the video
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    # Loop through each face in this frame of video
+    # Loop through each face in this frame of the video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
